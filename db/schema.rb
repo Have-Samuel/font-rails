73a9dfb34007e3cc2107215845c5fb3687d00e8c
Have Samuel <samiehave9@gmail.com>
2026-02-21 07:11:31 +0300

    Editing the file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_19_202825) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_21_034858) do
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -21,6 +21,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_19_202825) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 
   create_table "imstalls", force: :cascade do |t|
