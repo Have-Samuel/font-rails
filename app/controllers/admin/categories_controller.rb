cb323d574636bc7fca3b9c3a51300fbd96ac00dd
Have Samuel <samiehave9@gmail.com>
2026-02-26 06:17:27 +0300

    Corrections

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -62,8 +62,8 @@ class Admin::CategoriesController < AdminController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_admin_category
-      # @admin_category = Category.find(params.expect(:id))
-      @admin_category = Category.find(params[:id])
+      @admin_category = Category.find(params.expect(:id))
+      # @admin_category = Category.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
