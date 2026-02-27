811a3ec1b1ccc36304403ddbe54d235c2e9a18e7
Have Samuel <samiehave9@gmail.com>
2026-02-27 23:45:11 +0300

    Fixing Errors

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "devise/orm/active_record"
-
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -16,7 +14,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '0110a30ff7eb4774bcb12362047c6c79a8384045e0ab80876765e73d9ef3995281800c5a226bc43bb907cc25f3951df39abaa44234d7f9cd92e421315367ab28'
+  # config.secret_key = 'c5bc3bff6e417c7328dd00e3d4c1edbd3dd66b6ac3f0b663874abe0b43b360f4c8b916c84e34cedacddbb7734b799d7627614e8a777cd50e0e1460a488a9c6ef'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -26,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -38,7 +36,7 @@ Devise.setup do |config|
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
-  require "devise/orm/active_record"
+  require 'devise/orm/active_record'
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
@@ -60,12 +58,12 @@ Devise.setup do |config|
   # Configure which authentication keys should be case-insensitive.
   # These keys will be downcased upon creating or modifying a user and when used
   # to authenticate or find a user. Default is :email.
-  config.case_insensitive_keys = [ :email ]
+  config.case_insensitive_keys = [:email]
 
   # Configure which authentication keys should have whitespace stripped.
   # These keys will have whitespace before and after removed upon creating or
   # modifying a user and when used to authenticate or find a user. Default is :email.
-  config.strip_whitespace_keys = [ :email ]
+  config.strip_whitespace_keys = [:email]
 
   # Tell if authentication through request.params is enabled. True by default.
   # It can be set to an array that will enable params authentication only for the
@@ -99,7 +97,7 @@ Devise.setup do |config|
   # Notice that if you are skipping storage for all authentication paths, you
   # may want to disable generating routes to Devise's sessions controller by
   # passing skip: :sessions to `devise_for` in your config/routes.rb
-  config.skip_session_storage = [ :http_auth ]
+  config.skip_session_storage = [:http_auth]
 
   # By default, Devise cleans up the CSRF token on authentication to
   # avoid CSRF token fixation attacks. This means that, when using AJAX
@@ -128,7 +126,7 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 12
 
   # Set up a pepper to generate the hashed password.
-  # config.pepper = '3b8b87d0471cdf5ee49fb306f9fc5572dc57b3af125a3df0ac2cf984cfb45401c322dfa2b007569d27184039bef622a274a1a78e57a805cdccf299d87238ebd6'
+  # config.pepper = 'c0932ac330e5f3ea463f4c5eb9fe5e5b5b22f27001337bb31b0658419aa151ba7b93546e27e34ace541a72ca9225eba0a44103659f26c0b8f11f3a7fccecf6df'
 
   # Send a notification to the original email when the user's email is changed.
   # config.send_email_changed_notification = false
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :products
-      resources :categories
-  end
+  # namespace :admin do
+  #   resources :products
+  #     resources :categories
+  # end
+
   devise_for :admins
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -22,6 +24,6 @@ Rails.application.routes.draw do
     root to: "admin#index", as: :admin_root
   end
 
-   # For admin index route:
+  # For admin index route:
    get "admin" => "admin#index"
 end
